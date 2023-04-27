f7bc0a93e4a6957a90e4807dd13ccdfe482a3a0c
Johansogaard <Johansogaardjorgensen@gmail.com>
2023-04-27 21:06:09 +0200

    JJ working on jason

---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/model/templates/CommandCardFieldTemplate.java
@@ -0,0 +1,28 @@
+package dk.dtu.compute.se.pisd.roborally.fileaccess.model.templates;
+
+import dk.dtu.compute.se.pisd.roborally.model.Board;
+import dk.dtu.compute.se.pisd.roborally.model.CommandCard;
+import dk.dtu.compute.se.pisd.roborally.model.CommandCardField;
+import dk.dtu.compute.se.pisd.roborally.model.Player;
+
+public class CommandCardFieldTemplate {
+
+
+    private CommandCard card;
+
+    private boolean visible;
+    public CommandCardFieldTemplate fromCommandCardField(CommandCardField commandCardField)
+    {
+        this.visible = commandCardField.isVisible();
+        this.card = commandCardField.getCard();
+
+        return this;
+    }
+    public CommandCardField toCommandCardField(Player player)
+    {
+        CommandCardField commandCardField = new CommandCardField(player);
+        commandCardField.setVisible(visible);
+        commandCardField.setCard(card);
+        return commandCardField;
+    }
+}
---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/model/templates/PlayerTemplate.java
@@ -0,0 +1,60 @@
+package dk.dtu.compute.se.pisd.roborally.fileaccess.model.templates;
+
+import dk.dtu.compute.se.pisd.roborally.model.*;
+
+import static dk.dtu.compute.se.pisd.roborally.model.Heading.SOUTH;
+
+public class PlayerTemplate {
+    public int no;
+    private String name;
+    private String color;
+    private Heading heading = SOUTH;
+
+    private CommandCardFieldTemplate[] program;
+    private CommandCardFieldTemplate[] cards;
+    public PlayerTemplate fromPlayer(Player player)
+    {
+        this.no = player.no;
+        this.name = player.getName();
+        this.color = player.getColor();
+        this.heading = player.getHeading();
+        program = new CommandCardFieldTemplate[player.getProgram().length];
+        for (int i = 0;i<player.getProgram().length;i++)
+        {
+            program[i] = new CommandCardFieldTemplate().fromCommandCardField(player.getProgramField(i));
+
+        }
+        cards = new CommandCardFieldTemplate[player.getCards().length];
+        for (int i = 0;i<player.getCards().length;i++)
+        {
+            cards[i] = new CommandCardFieldTemplate().fromCommandCardField(player.getCardField(i));
+
+        }
+
+        return this;
+
+    }
+    public Player toPlayer(Board board)
+    {
+        Player player = new Player(board,this.color,this.name);
+        player.no = this.no;
+        player.setHeading(this.heading);
+        CommandCardField[] programToPlayer = new CommandCardField[program.length];
+        for (int i = 0;i<program.length;i++)
+        {
+            programToPlayer[i] = program[i].toCommandCardField(player);
+
+        }
+        player.setProgram(programToPlayer);
+        CommandCardField[] cardToPlayer = new CommandCardField[cards.length];
+        for (int i = 0;i<cards.length;i++)
+        {
+            cardToPlayer[i] = cards[i].toCommandCardField(player);
+
+        }
+        player.setCards(cardToPlayer);
+
+
+        return player;
+    }
+}
