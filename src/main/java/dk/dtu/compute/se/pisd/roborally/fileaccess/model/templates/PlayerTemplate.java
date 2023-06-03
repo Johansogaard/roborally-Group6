16811ce52e66b9da5dde23795d20b2e8845b28ef
Johansogaard <Johansogaardjorgensen@gmail.com>
2023-06-03 13:16:13 +0200

    Gamesave works

--- src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/model/templates/PlayerTemplate.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/model/templates/PlayerTemplate.java
@@ -9,7 +9,7 @@ public class PlayerTemplate {
     private String name;
     private String color;
     private Heading heading = SOUTH;
-
+    private int[] space;
     private CommandCardFieldTemplate[] program;
     private CommandCardFieldTemplate[] cards;
     public PlayerTemplate fromPlayer(Player player)
@@ -18,16 +18,21 @@ public class PlayerTemplate {
         this.name = player.getName();
         this.color = player.getColor();
         this.heading = player.getHeading();
-        program = new CommandCardFieldTemplate[player.getProgram().length];
+        this.space = new int[]{player.getSpace().x,player.getSpace().y};
+        this.program = new CommandCardFieldTemplate[player.getProgram().length];
+
+        CommandCardField[] commandCardField = player.getProgram();
         for (int i = 0;i<player.getProgram().length;i++)
         {
-            program[i] = new CommandCardFieldTemplate().fromCommandCardField(player.getProgramField(i));
+            program[i] = new CommandCardFieldTemplate().fromCommandCardField(commandCardField[i]);
 
         }
-        cards = new CommandCardFieldTemplate[player.getCards().length];
+        this.cards = new CommandCardFieldTemplate[player.getCards().length];
+
+        CommandCardField[] cardFields = player.getCards();
         for (int i = 0;i<player.getCards().length;i++)
         {
-            cards[i] = new CommandCardFieldTemplate().fromCommandCardField(player.getCardField(i));
+            cards[i] = new CommandCardFieldTemplate().fromCommandCardField(cardFields[i]);
 
         }
 
@@ -39,6 +44,8 @@ public class PlayerTemplate {
         Player player = new Player(board,this.color,this.name);
         player.no = this.no;
         player.setHeading(this.heading);
+
+
         CommandCardField[] programToPlayer = new CommandCardField[program.length];
         for (int i = 0;i<program.length;i++)
         {
