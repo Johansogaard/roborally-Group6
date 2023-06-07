9d7f0efeca1f302315672477fe4aa9ca1d241d7a
Anders Jefsen <andersjeff397@gmail.com>
2023-06-07 11:19:20 +0200

    savegame til server integreret

--- src/main/java/dk/dtu/compute/se/pisd/roborally/controller/AppController.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/AppController.java
@@ -80,7 +80,7 @@ import javafx.scene.control.TextInputDialog;
 public class AppController implements Observer {
     private static final Logger logger = LoggerFactory.getLogger(AppController.class);
 
-    private static final String BASE_URL = "http://localhost:8083/api/game";
+    private static final String BASE_URL = "http://localhost:8084/api/game";
 
     final private List<Integer> PLAYER_NUMBER_OPTIONS = Arrays.asList(2, 3, 4, 5, 6);
     final private List<String> PLAYER_COLORS = Arrays.asList("red", "green", "blue", "orange", "grey", "magenta");
