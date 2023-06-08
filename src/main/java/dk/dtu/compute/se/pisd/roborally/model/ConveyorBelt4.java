5df25b1a0ca4f44f90923df46a566681d72140f8
mikkel <mtschwarz9@gmail.com>
2023-06-08 12:47:10 +0200

    single directional conveyor belt og billede tilføjet

---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/ConveyorBelt4.java
@@ -0,0 +1,45 @@
+package dk.dtu.compute.se.pisd.roborally.model;
+
+import dk.dtu.compute.se.pisd.roborally.controller.GameController;
+import org.jetbrains.annotations.NotNull;
+
+public class ConveyorBelt4 implements FieldAction {
+
+    private Heading heading = Heading.EAST;
+
+    public Heading getHeading() {
+        return heading;
+    }
+
+    public FieldAction action;
+
+    public void setHeading(Heading heading) {
+        this.heading = heading;
+    }
+
+
+    @Override
+    public boolean doAction(@NotNull GameController gameController, @NotNull Space space) {
+        Player currentPlayer = space.getPlayer();
+
+        currentPlayer.setHeading(this.heading);
+
+        // Check if the player can move forward
+        Space neighbourSpace = space.board.getNeighbour(space, heading);
+
+        if (neighbourSpace.getPlayer() != null) {
+            return false;
+        } else {
+
+            for (FieldAction action : neighbourSpace.actions) {
+                if (action instanceof ConveyorBelt && ((ConveyorBelt) action).getHeading() != this.heading.opposite()) {
+                    currentPlayer.setSpace(neighbourSpace);
+                }
+            }
+            // if there's no adjecent conveyorbelts for the current player
+            currentPlayer.setSpace(neighbourSpace);
+
+            return true;
+        }
+    }
+}
